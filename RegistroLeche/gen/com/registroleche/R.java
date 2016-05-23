4c43901d22144a3b498edb56e82f546068c87401
UTC1992 <omar.guanoluisa25@gmail.com>
2016-05-22 23:21:19 -0500

    Crecion de activity  para login

--- RegistroLeche/gen/com/registroleche/R.java
+++ RegistroLeche/gen/com/registroleche/R.java
@@ -1116,13 +1116,15 @@ containing a value of this type.
         public static final int abc_screen=0x7f030015;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030016;
         public static final int abc_search_view=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int activity_registro_user=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_login_user=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int activity_registro_user=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
-        public static final int registro_user=0x7f0c0001;
+        public static final int login_user=0x7f0c0000;
+        public static final int main=0x7f0c0001;
+        public static final int registro_user=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1168,6 +1170,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_login_user=0x7f0a0011;
         public static final int title_activity_registro_user=0x7f0a0010;
     }
     public static final class style {
